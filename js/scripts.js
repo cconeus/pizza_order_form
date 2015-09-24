4b47b0d2c7ba20d05888f6996ef48e49dc87f91d
John <student@epicodus.com>
2015-09-24 10:54:10 -0700

    added checked example

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 function PizzaOrder(pizzaSize, pizzaToppings, quantity) {
-    debugger;
-    this.pizzaSize = pizzaSize,
-    this.pizzaToppings = [],
-    this.quantity = ""
+  debugger;
+  this.pizzaSize = pizzaSize,
+  this.pizzaToppings = [],
+  this.quantity = ""
 };
 
 function PizzaToppings(mushroom, onion, black_olives, sausage, pepperoni) {
@@ -10,64 +10,70 @@ function PizzaToppings(mushroom, onion, black_olives, sausage, pepperoni) {
 }
 
 $(document).ready(function() {
-    $("#add-pizza").click(function() {
-   $("#new-pizza").append( '<div class="form-group">' +
-       '<label for="pizza-size">Select a size:</label>' +
-       '<select class="newSize">' +
-       '<option value="Medium">Medium</option>' +
-       '<option value="Large">Large</option>' +
-       '<option value="Family">Family</option>' +
-       '</select>' +
-       '</div>' +
-       '<div class="form-group">' +
-       '<label for="pizza-toppings">Select your toppings:</label><br>' +
-       '<label><input type="checkbox" name="mushrooms" value="1" /> mushrooms</label>' +
-       '<label><input type="checkbox" name="onions" value="1" /> onions</label>' +
-       '<label><input type="checkbox" name="black_olives" value="1" /> black olives</label>' +
-       '<label><input type="checkbox" name="sausage" value="1" /> sausage</label>' +
-       '<label><input type="checkbox" name="pepperoni" value="1" /> pepperoni</label>' +
-       '</div>' +
-       '<div class="form-group">' +
-        '<label for="pizza-size">Select qty:</label>' +
-        '<select class="quantity">' +
-         '<option value="1">1</option>' +
-         '<option value="2">2</option>' +
-         '<option value="3">3</option>' +
-         '<option value="4">4</option>' +
-         '<option value="5">5</option>' +
-         '<option value="6">6</option>' +
-         '<option value="7">7</option>' +
-         '<option value="8">8</option>' +
-         '<option value="9">9</option>' +
-         '<option value="10">10</option>' +
-         '</select>' +
-         '</div>');
-    });
+  $("#add-pizza").click(function() {
+    $("#new-pizza").append( '<div class="form-group">' +
+    '<label for="pizza-size">Select a size:</label>' +
+    '<select class="newSize">' +
+    '<option value="Medium">Medium</option>' +
+    '<option value="Large">Large</option>' +
+    '<option value="Family">Family</option>' +
+    '</select>' +
+    '</div>' +
+    '<div class="form-group">' +
+    '<label for="pizza-toppings">Select your toppings:</label><br>' +
+    '<label><input type="checkbox" name="mushrooms" value="1" /> mushrooms</label>' +
+    '<label><input type="checkbox" name="onions" value="1" /> onions</label>' +
+    '<label><input type="checkbox" name="black_olives" value="1" /> black olives</label>' +
+    '<label><input type="checkbox" name="sausage" value="1" /> sausage</label>' +
+    '<label><input type="checkbox" name="pepperoni" value="1" /> pepperoni</label>' +
+    '</div>' +
+    '<div class="form-group">' +
+    '<label for="pizza-size">Select qty:</label>' +
+    '<select class="quantity">' +
+    '<option value="1">1</option>' +
+    '<option value="2">2</option>' +
+    '<option value="3">3</option>' +
+    '<option value="4">4</option>' +
+    '<option value="5">5</option>' +
+    '<option value="6">6</option>' +
+    '<option value="7">7</option>' +
+    '<option value="8">8</option>' +
+    '<option value="9">9</option>' +
+    '<option value="10">10</option>' +
+    '</select>' +
+    '</div>');
+  });
 
-    $("form#new-pizza").submit(function(event) {
-        event.preventDefault();
+  $("form#new-pizza").submit(function(event) {
+    event.preventDefault();
 
-        var inputtedPizzaSize = $("input#new-size").val();
-        var inputtedQuantity = $("input#quantity").val();
-        var mushroom = $("input[name='mushroom']:checked").val();
-        var onion = $("input[name='onion']:checked").val();
-        var black_olives = $("input[name='black_olives']:checked").val();
-        var sausage = $("input[name='sausage']:checked").val();
-        var pepperoni = $("input[name='pepperoni']:checked").val();
+    var inputtedPizzaSize = $("input#new-size").val();
+    var inputtedQuantity = $("input#quantity").val();
+    var mushroom = $("input[name='mushroom']:checked").val();
+    var onion = $("input#onion").val();
+//example of check validation
+        if ($('#onion').is(':checked')){
+        alert('YAY!');
+    };
 
-        var newPizzaOrder = new PizzaOrder(inputtedPizzaSize, newPizzaToppings, inputtedQuantity);
+    console.log(onion);
+    var black_olives = $("input[name='black_olives']:checked").val();
+    var sausage = $("input[name='sausage']:checked").val();
+    var pepperoni = $("input[name='pepperoni']:checked").val();
 
-        var newPizzaToppings = new PizzaToppings(mushroom, onion, black_olives, sausage, pepperoni);
-        console.log(newPizzaToppings);
-    });
-    // $("ul#show-pizza").append("<li><span class='order'" + newPizzaOrder.pizzaToppings() + "</span></li>");
+    var newPizzaOrder = new PizzaOrder(inputtedPizzaSize, newPizzaToppings, inputtedQuantity);
 
+    var newPizzaToppings = new PizzaToppings(mushroom, onion, black_olives, sausage, pepperoni);
+    console.log(newPizzaToppings);
     $(".order").last().show(function() {
-        $("#show-pizza").fadeIn(500);
+      $("#show-pizza").fadeIn(500);
 
-        $("#show-order h2").text(newPizzaOrder.pizzaSize);
-        $("#pizza-size").text(newPizzaOrder.pizzaSize);
-        $("#pizza-toppings").text(newPizzaOrder.pizzaToppings);
-        $("#quantity").text(newPizzaOrder.quantity);
+      $("#show-order h2").text(newPizzaOrder.pizzaSize);
+      $("#pizza-size").text(newPizzaOrder.pizzaSize);
+      $("#pizza-toppings").text(newPizzaOrder.pizzaToppings);
+      $("#quantity").text(newPizzaOrder.quantity);
     })
+  });
+  // $("ul#show-pizza").append("<li><span class='order'" + newPizzaOrder.pizzaToppings() + "</span></li>");
+
 });
